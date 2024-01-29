7ffe599be5d4f7e4aa3ef586f4d717204e9e36ca
Владислава Морякова <vladislavamoryakova@yandex.ru>
2024-01-29 20:05:42 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static string[] SplitText(string text)
+        {
+            char[] chars = text.ToCharArray();
+            int count = 0;
+            /// Подсчет количества пробелов в тексте
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == ' ')
+                {
+                    count++;
+                }
+            }
+
+            string[] str = new string[count + 1];
+            int indexStart = 0;
+            int t = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    str[t++] = text.Substring(indexStart, i- indexStart);
+                    indexStart = ++i;                   
+                }
+                else if (i == text.Length-1) str[t] = text.Substring(indexStart);
+            }
+
+            //string[] str = text.Split(' ');  
+            return str;
+        }
+
+        static void GetSplitText(string[] str)
+        {
+            WriteLine("Слова в предложении после разделения: ");
+            foreach (string e in str)
+                {
+                    WriteLine(e);
+                }
+        }
+        static void Main(string[] args)
+        
+        {
+            WriteLine("Введите предложение: ");
+            string proposal = ReadLine();
+            string[] newText = SplitText(proposal);
+            GetSplitText(newText);
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static string Reverse(string text)
+        {
+            string[] splitText = SplitText(text);
+            string reversString = string.Empty;
+            for (int i = splitText.Length - 1; i >= 0; i--)
+            {
+                reversString += splitText[i] + ' ';
+            }
+            GetReverse(reversString);
+            return reversString;
+        }
+
+        private static void GetReverse(string text)
+        {
+            WriteLine("Предложение наоборот: ");
+            WriteLine(text);
+        }
+
+        public static string[] SplitText(string txt)
+        {
+            string[] str = txt.Split(' ');
+            return str;
+        }
+        static void Main(string[] args)
+
+        {
+            WriteLine("Введите предложение: ");
+            string proposal = ReadLine();
+            Reverse(proposal);
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
